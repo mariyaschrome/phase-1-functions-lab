8e29c403cf3cc922a351149f5d0d99ecc9228591
Mariya <mariaomware@gmail.com>
2024-03-19 10:20:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickuplocation) {
+    const hqlocation = 42;
+    return Math.abs(pickuplocation-hqlocation);
+}
+
+function distanceFromHqInFeet(pickuplocation)  {
+    const blocks= distanceFromHqInBlocks(pickuplocation);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocks= Math.abs(destination-start);
+    return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination); 
+    let fare;
+  
+    if (distance <= 400) {
+      fare = 0.00; 
+    } else if (distance > 400 && distance <= 2000) {
+      fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      fare = 25.00;
+    } else {
+      return 'cannot travel that far';
+    }
+
+    return parseFloat(fare.toFixed(2));
+}
